c08fc6a6146ea575551b05eba21ff973d5e1dadb
Adin Ashby <adin.ashby@gmail.com>
2023-03-23 01:01:46 -0400

    first commit

---
+++ Midterm.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+/**
+ * DO NOT MODIFY THE MAIN METHOD.
+ *
+ * @author adinashby
+ */
+
+void main(List<String> arguments) {
+  // Question 1
+  print(minWindow("ADOBECODEBANC", "DOC"));
+  // COD
+
+  print(minWindow("ADOBECODEBANC", "ABC"));
+  // BANC
+
+  print(minWindow("a", "a"));
+  // a
+
+  print(minWindow("a", "aa"));
+  // (empty string)
+}
+
+/**
+ * Please refer to the README file for question(s) description
+ */
+
+String minWindow(String s, String t) {
+  return "";
+}
